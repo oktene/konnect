0207def83c3a1969f090d6f444cd10895b9fa2b9
Lazaro Miranda <contatolazaromiranda@gmail.com>
2024-09-04 18:17:25 -0300

    feat: Initialize authentication

---
+++ api/src/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/modules/user/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+  ) {}
+
+  // async signIn(
+  //   username: string,
+  //   pass: string,
+  // ): Promise<{ access_token: string }> {
+  //   const user = await this.userService.findOne(username);
+  //   if (user?.password !== pass) {
+  //     throw new UnauthorizedException();
+  //   }
+  //   const payload = { sub: user.userId, username: user.username };
+  //   return {
+  //     access_token: await this.jwtService.signAsync(payload),
+  //   };
+  // }
+}
