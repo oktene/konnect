d3d41f061550e96cae3e800321e6a2704c21ecd0
oktene <tech@oktene.com.br>
2024-09-09 16:27:59 -0300

    delete: remove incorrect folder

--- address/address.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AddressService } from './address.service';
-import { AddressController } from './address.controller';
-
-@Module({
-  controllers: [AddressController],
-  providers: [AddressService]
-})
-export class AddressModule {}
