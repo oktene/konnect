4ea979e640f1c301fa38d931f3bc1ea307e7a2ca
felipemarq <felipeshay2000@gmail.com>
2024-09-09 16:14:35 -0300

    fix: :bug: App Guard fixed

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,16 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { UserModule } from './modules/user/user.module';
 import { CompanyModule } from './modules/company/company.module';
 import { ProposalModule } from './modules/proposal/proposal.module';
 import { OpportunityModule } from './modules/opportunity/opportunity.module';
-import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './modules/auth/auth.guard';
+
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   imports: [
     UserModule,
     CompanyModule,
