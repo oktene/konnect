9789798684f84808b743b728ff932ab490413d4e
oktene <tech@oktene.com.br>
2024-09-09 23:22:58 +0000

    feat: Add responses to endpoints

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,28 +1,43 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { SigninDto } from './dto/signin.dto';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { IsPublic } from 'src/shared/decorators/isPublic';
+import { ResponseHandlerService } from 'src/shared/handlers/responseHandler.service';
 
-@ApiBearerAuth()
-@ApiTags('User')
-@Controller('auth')
 @IsPublic()
+@ApiTags('Auth')
+@Controller('')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly responseHandler: ResponseHandlerService
+  ) { }
 
-  @Post('signup')
-  @ApiOperation({ summary: 'Create an user' })
+  @Post('sign-up')
+  @ApiOperation({ summary: 'Register an user' })
   @ApiBody({ type: SignupDto })
-  create(@Body() signupDto: SignupDto) {
-    return this.authService.signup(signupDto);
+  @ApiResponse({ status: 201, description: 'The user and company has been successfully created.' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 403, description: `You don't have permission to access '/signUp' on the server.` })
+  async signUp(@Body() signupDto: SignupDto) {
+    try {
+      const user = this.authService.signup(signupDto);
+      return this.responseHandler.success(user, 'User registered successfully');
+    } catch (error) {
+      return this.responseHandler.error('Authentication failed', 401);
+      throw new HttpException('Registration failed', HttpStatus.BAD_REQUEST);
+    }
   }
 
-  @ApiOperation({ summary: 'Authenticate a user' })
-  @ApiBody({ type: SignupDto })
-  @Post('signin')
-  authenticate(@Body() signinDto: SigninDto) {
+  @Post('sign-in')
+  @ApiOperation({ summary: 'Authenticate an user' })
+  @ApiBody({ type: SigninDto })
+  @ApiResponse({ status: 201, description: 'The user and company has been successfully created.' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 403, description: `You don't have permission to access '/signIn' on the server.` })
+  signIn(@Body() signinDto: SigninDto) {
     return this.authService.signin(signinDto);
   }
 }
