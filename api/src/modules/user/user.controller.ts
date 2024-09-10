b89076dd5627d10b3af6ebb12873148e9e4acd82
oktene <tech@oktene.com.br>
2024-09-10 19:02:36 +0000

    feat: Add permission Editor in endpoints

--- api/src/modules/user/user.controller.ts
+++ api/src/modules/user/user.controller.ts
@@ -6,42 +6,52 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { PermissionsLevelsGuard } from 'src/shared/guards/permissionLevels.guard';
+import { Permissions } from 'src/shared/decorators/permission.decorator';
+import { PermissionLevel as UserPermission } from 'src/shared/enums/permissionLevel.enum';
 
 @ApiBearerAuth()
 @ApiTags('User')
 @Controller('user')
+@UseGuards(PermissionsLevelsGuard)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @Permissions(UserPermission.EDITOR)
   @ApiOperation({ summary: 'Get all users in the Konnect' })
   async getAll() {
     return this.userService.getAll();
   }
 
   @Get(':id')
+  @Permissions(UserPermission.EDITOR)
   @ApiOperation({ summary: 'Get an user in the Konnect by ID' })
   async getOneById(@Param('id') userId: string) {
     return await this.userService.getOneById(userId);
   }
 
   @Get(':email')
+  @Permissions(UserPermission.EDITOR)
   @ApiOperation({ summary: 'Get an user in the Konnect by Email' })
   async getOneByEmail(@Param('email') userEmail: string) {
     return this.userService.getOneByEmail(decodeURIComponent(userEmail));
   }
 
   @Patch(':id')
+  @Permissions(UserPermission.EDITOR || UserPermission.ADMIN || UserPermission.USER)
   @ApiOperation({ summary: 'Update an user' })
   async update(@Param('id') userId: string, @Body() updateUserDto: UpdateUserDto) {
     return await this.userService.update(userId, updateUserDto);
   }
 
   @Delete(':id')
+  @Permissions(UserPermission.EDITOR)
   @ApiOperation({ summary: 'Delete an user' })
   async remove(@Param('id') userId: string) {
     return await this.userService.delete(userId);
