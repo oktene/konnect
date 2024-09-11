2d53e7561fee4529c58ceca0c09b3cc37c2b9eeb
oktene <tech@oktene.com.br>
2024-09-11 18:04:04 +0000

    feat: Add Address and Proposal modules

--- api/src/shared/database/database.module.ts
+++ api/src/shared/database/database.module.ts
@@ -5,10 +5,12 @@ import { CompanyRepository } from './repositories/company.repositories';
 import { PrismaService } from './prisma.service';
 import { CategoryRepository } from './repositories/category.repositories';
 import { SubcategoryRepository } from './repositories/subcategory.repositories';
+import { AddressModule } from 'src/modules/address/address.module';
+import { ProposalModule } from 'src/modules/proposal/proposal.module';
 
 @Global()
 @Module({
-  providers: [UserRepository, CompanyRepository, CategoryRepository, SubcategoryRepository, OpportunityRepository, PrismaService],
-  exports: [UserRepository, CompanyRepository, CategoryRepository, SubcategoryRepository, OpportunityRepository, PrismaService],
+  providers: [UserRepository, CompanyRepository, CategoryRepository, SubcategoryRepository, OpportunityRepository, AddressModule, ProposalModule, PrismaService],
+  exports: [UserRepository, CompanyRepository, CategoryRepository, SubcategoryRepository, OpportunityRepository, AddressModule, ProposalModule, PrismaService],
 })
 export class DatabaseModule {}
