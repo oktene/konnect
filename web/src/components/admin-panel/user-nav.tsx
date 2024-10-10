9a3034ff8d3b904b85400c5b29c0d964d5ea4ee1
mirndlazaro <contatolazaromiranda@gmail.com>
2024-10-10 13:25:18 -0300

    feat(Auth): :bug: Fix the user's infos

--- web/src/components/admin-panel/user-nav.tsx
+++ web/src/components/admin-panel/user-nav.tsx
@@ -25,7 +25,6 @@ import { useAuth } from "@/hooks/useAuth";
 
 export function UserNav() {
   const {user, signout} = useAuth()
-  console.log("user", user)
   return (
     <DropdownMenu>
       <TooltipProvider >
@@ -50,7 +49,7 @@ export function UserNav() {
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-semibold leading-none">Oktene</p>
+            <p className="text-sm font-semibold leading-none">{user?.company.name}</p>
             <p className="text-xs leading-none text-muted-foreground">
               {user?.name}
             </p>
